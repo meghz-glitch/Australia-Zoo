0ee0d6d839434748908d0a341a8970d59b51e10f
Megha Albin <megha.albin@edu.futuregames.se>
2025-05-27 21:33:49 +0200

    i add some code

--- src/components/AnimalCard/animalCard.jsx
+++ src/components/AnimalCard/animalCard.jsx
@@ -1,38 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../components/AnimalCard/animalCard.module.css";
 
-const AnimalCard = ({ animal, showGroupLink = false }) => {
-  const [expanded, setExpanded] = useState(false);
-
+const AnimalCard = ({ animal, showGroupLink = false, onReadMore }) => {
   const shortDesc = animal.description.slice(0, 200) + "...";
 
   return (
     <div className={styles.card}>
-      <img
-        src={animal.image}
-        alt={animal.name}
-        className={expanded ? styles.fullImage : styles.thumb}
-      />
+      <img src={animal.image} alt={animal.name} className={styles.thumb} />
 
       <h3 className={styles.name}>{animal.name}</h3>
-      <p className={styles.description}>
-        {expanded ? animal.description : shortDesc}
-      </p>
+      <p className={styles.description}>{shortDesc}</p>
       <p>
         <strong>Food:</strong> {animal.food}
       </p>
       {showGroupLink && (
-        <Link to={`/${animal.group}`} className={styles.groupLink}>
+        <Link to={`⁠/${animal.group}`} className={styles.groupLink}>
           {animal.group}
         </Link>
       )}
       <br />
-      <button
-        className={styles.readMore}
-        onClick={() => setExpanded(!expanded)}
-      >
-        {expanded ? "Show Less" : "Read More"}
+      <button className={styles.readMore} onClick={onReadMore}>
+        Read More
       </button>
     </div>
   );
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,20 +4,24 @@ import { animals } from "../../data/animals";
 import Sidebar from "../../components/Sidebar/sidebar";
 import AnimalCard from "../../components/AnimalCard/animalCard";
 import AnimalDetailModal from "../../components/AnimalDetailModal/animalDetailModal";
- 
+
 const Home = () => {
   const [activeAnimalName, setActiveAnimalName] = useState(null);
   const [showModal, setShowModal] = useState(false);
- 
+
   const handleAnimalClick = (name) => {
     setActiveAnimalName((prev) => (prev === name ? null : name));
     setShowModal(false);
   };
- 
+
   const handleReadMore = () => {
     setShowModal(true);
   };
- 
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   const sidebar = (
     <Sidebar
       animals={animals}
@@ -25,18 +29,15 @@ const Home = () => {
       onAnimalClick={handleAnimalClick}
     />
   );
- 
+
   const activeAnimal = animals.find(
     (animal) => animal.name === activeAnimalName
   );
- 
+
   return (
-    <PageLayout sidebar={sidebar}>
+    <PageLayout sidebar={sidebar} showSidebar={!showModal}>
       {showModal ? (
-        <AnimalDetailModal
-          animal={activeAnimal}
-          onClose={() => setShowModal(false)}
-        />
+        <AnimalDetailModal animal={activeAnimal} onClose={handleCloseModal} />
       ) : activeAnimal ? (
         <AnimalCard
           animal={activeAnimal}
@@ -49,5 +50,5 @@ const Home = () => {
     </PageLayout>
   );
 };
- 
+
 export default Home;
