7ab747d5d9c09d6e7793ce0e808ab7880318991c
Megha Albin <megha.albin@edu.futuregames.se>
2025-05-30 15:03:45 +0200

    FIX

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -4,13 +4,15 @@ import styles from '../Sidebar/sidebar.module.css';
 const Sidebar = ({ animals, activeAnimal, onAnimalClick }) => (
   <ul className={styles.sidebarList}>
     {animals.map((animal) => (
-      <li
-        key={animal.name}
-        className={activeAnimal === animal.name ? styles.active : ''}
-        onClick={() => onAnimalClick(animal.name)}
-      >
-        {animal.name}
-      </li>
+    <li
+    key={animal.name}
+    className={`${styles.animalItem} ${
+      activeAnimal === animal.name ? styles.active : ''
+    }`}
+    onClick={() => onAnimalClick(animal.name)}
+  >
+    {animal.name}
+  </li>
     ))}
   </ul>
 );
