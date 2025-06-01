bc862f0609aa6c70596d521d246a1f206a1a91e5
Megha Albin <megha.albin@edu.futuregames.se>
2025-06-01 13:32:21 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Home from '../src/pages/Home/home';
 import Mammals from '../src/pages/Mammals/mammals';
 import Birds from '../src/pages/Birds/birds';
 import Reptiles from '../src/pages/Reptiles/reptiles';
+import Layouts from '../src/layouts/index';
 
 function App() {
   return (
     
    
     <Routes>
-      
+      <Route element={<Layouts />}></Route>
       <Route path="/" element={<Home />} />
       <Route path="/mammals" element={<Mammals />} />
       <Route path="/birds" element={<Birds />} />
